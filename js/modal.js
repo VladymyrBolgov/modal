26cf3d242f886d5e811adc8efa41b699fbf1db11
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-09 22:05:47 +0300

    t

--- js/modal.js
+++ js/modal.js
@@ -3,18 +3,19 @@ const openBtn = document.querySelector(".open");
 const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close");
 
-/*function showModal () {// hide в css скрывает модалку, удаляя hide
-  overlay.classList.remove('hide')// открывается модалка
+/*function showModal () {/ / hide в css скрывает модалку, удаляя hide
+  overlay.classList.remove('hide') // открывается модалка
   }
 function hideModal() {
   overlay.classList.add('hide');
    }*/
-/*function toggleModal() {//toggleModal открыть и закрыть
+/*function toggleModal() { //toggleModal открыть и закрыть
     overlay.classList.toggle('hide')
   }
 openBtn.addEventListener('click', toggleModal);
 closeBtn.addEventListener('click', toggleModal);*/
 
+// Второй вариант с клавиатурой
 const openBtn = document.querySelector(".open");
 const overlay = document.querySelector(".overlay");
 const closeBtn = document.querySelector(".close");
