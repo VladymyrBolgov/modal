408abe719444dacf624b68dc5fed34d08437563f
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-09 16:31:01 +0300

    b

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,20 @@
-const form = document.querySelector(".form");
-const email = document.querySelector(".email");
-const password = document.querySelector(".password");
 
-function handleSubmit(e) {
-  e.preventDefault();
-  const formData = {
-    email: email.value,
-    password: password.value,
-  };
-  console.log(formData);
-  form.reset();
+const openBtn = document.querySelector(".open");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close");
+
+function toggleModal(event) {
+  const { code } = event;
+  if (code === 'Escape') {
+    if (overlay.classList.contains('hide')) {
+      return;
+    }
+    overlay.classList.toggle('hide');
+    return;
+  }
+  overlay.classList.toggle('hide');
 }
+openBtn.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
 
-form.addEventListener("submit", handleSubmit);
+addEventListener('keydown', toggleModal);
