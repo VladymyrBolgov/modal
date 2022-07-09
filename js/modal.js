dca09022103b2d76cf188cd98a088fc61b200725
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-07-09 16:00:32 +0300

    f

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,15 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-})();
+const form = document.querySelector(".form");
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(formData);
+  form.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
